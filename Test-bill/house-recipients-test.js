6c2a21edd8b530e63cb80d97e1e638fc7a3e6fe2
campamentocemi <campamentocemi@gmail.com>
2026-05-15 23:45:37 -0400

    Create house-recipients-test.js

---
+++ Test-bill/house-recipients-test.js
@@ -0,0 +1,272 @@
+// House recipients
+// Generated from your compiled spreadsheet.
+// For each new bill/action, update the "vote" value as needed.
+// Recommended vote values: "A Favor", "En Contra", "Ausente", "Abstención".
+
+const HOUSE_RECIPIENTS = [
+  {
+    "name": "Burgos Muñiz, Lisie J.",
+    "email": "lburgos@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Gutiérrez Colón, Adriana",
+    "email": "agutierrez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Lebrón Robles, Nelie",
+    "email": "nlebron@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Márquez Lebrón, Denis",
+    "email": "dmarquez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Aponte Hernández, José F.",
+    "email": "japonte@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Carlo Acosta, Emilio",
+    "email": "ecarlo@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Charbonier Chinea, Eddie",
+    "email": "echarbonier@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Colón Rodríguez, Joe",
+    "email": "joecolon@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Del Valle Correa, Wanda",
+    "email": "wdelvalle@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Estevez Vélez, Sergio E.",
+    "email": "sestevez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Franqui Atiles, Joel I.",
+    "email": "jfranqui@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "González Aguayo, Elinette",
+    "email": "elgonzalez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "González González, Odalys",
+    "email": "odgonzalez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Hernández Concepción, José",
+    "email": "jhernandez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Jiménez Torres, Luis Josean",
+    "email": "luijimenez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Lebrón Rodríguez, Yashira",
+    "email": "ylebron@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "López Román, Roberto",
+    "email": "roblopez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Martínez Vázquez, Omayra Milagros",
+    "email": "omartinez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Medina Calderón, Carmen M",
+    "email": "cmedina@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Méndez Núñez, Carlos J.",
+    "email": "cmendez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Morey Noble, Ángel",
+    "email": "amorey@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Muriel Sánchez , Christian",
+    "email": "cmuriel@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Navarro Suárez, Jorge L.",
+    "email": "jnavarro@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Nieves Rosario, Jerry",
+    "email": "jnieves@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Ocasio Ramos, Ricardo Rey",
+    "email": "rocasio@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Pacheco Burgos, Félix Emmanuel",
+    "email": "fpacheco@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Parés Otero, Víctor L.",
+    "email": "vpares@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Peña Ramírez, Ángel R.",
+    "email": "anpena@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Pérez Cordero, José Jesús",
+    "email": "joperez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Pérez Ortiz, Luis",
+    "email": "lperez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Pérez Ramírez, Tatiana",
+    "email": "tperez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Ramos Rivera, María de Lourdes",
+    "email": "lramos@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Robles Rivera, Edgar",
+    "email": "erobles@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Rodríguez Aguiló, Gabriel",
+    "email": "gfrodriguez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Rodríguez Torres, Ensol Alexis",
+    "email": "enrodriguez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Román López, Wilson J.",
+    "email": "wroman@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Roque Gracia, Axel Francisco",
+    "email": "aroque@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Sanabria Colón , Fernando",
+    "email": "fsanabria@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Santiago Guzman, Pedro Julio",
+    "email": "psantiago@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Torres Zamora, José E.",
+    "email": "jotorres@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Feliciano Sánchez, Edgardo",
+    "email": "efeliciano@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Ferrer Santiago, Héctor E.",
+    "email": "hferrer@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Figueroa Acevedo, Reinaldo",
+    "email": "reifigueroa@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Fourquet Cordero, Ángel A.",
+    "email": "afourquet@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Hau, Gretchen",
+    "email": "ghau@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Higgins Cuadrado, Sol Yamiz",
+    "email": "shiggins@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Martínez Soto, Estrella",
+    "email": "emartinez@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Rivera Ruiz de Porras, Roberto",
+    "email": "rrivera@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Rosas Vargas, Lilibeth",
+    "email": "lrosas@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Torres Cruz, Ramón",
+    "email": "ramtorres@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Torres García, Domingo J.",
+    "email": "djtorres@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Varela Fernández, José M.",
+    "email": "jvarela@camara.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Vargas Laureano, Swanny",
+    "email": "svargas@camara.pr.gov",
+    "vote": ""
+  }
+];
