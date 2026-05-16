cc561a4365b9a5d819c0d6072cb66e7ee47c0e5f
campamentocemi <campamentocemi@gmail.com>
2026-05-16 10:09:45 -0400

    Add files via upload

---
+++ emailblast/senate-recipients.js
@@ -0,0 +1,147 @@
+// Senate recipients
+// Generated from your compiled spreadsheet.
+// For each new bill/action, update the "vote" value as needed.
+// Recommended vote values: "A Favor", "En Contra", "Ausente", "Abstención".
+
+const SENATE_RECIPIENTS = [
+  {
+    "name": "Rodríguez Veve, Joanne",
+    "email": "joarodriguez@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Matías Rosario, Gregorio",
+    "email": "gmatias@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Rivera Schatz, Thomas",
+    "email": "trivera@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Soto Aguilú, Roxanna I.",
+    "email": "risoto@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Toledo López, Ángel",
+    "email": "atoledo@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Morales Rodríguez, Juan Oscar",
+    "email": "jumorales@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Moran Trinidad, Nitza",
+    "email": "nmoran@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Padilla Alvelo, Migdalia",
+    "email": "mpadilla@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Ríos Santiago, Carmelo J.",
+    "email": "crios@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "González López, Héctor Gabriel",
+    "email": "hgonzalez@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Pérez Soto, Brenda",
+    "email": "bperez@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Román Rodríguez, Karen M.",
+    "email": "kmroman@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Rosa Ramos, Jeison",
+    "email": "jrosa@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Barlucea Rodríguez, Jamie",
+    "email": "jbarlucea@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Reyes Berríos, Wilmer",
+    "email": "wreyes@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Colón La Santa, Luis Daniel",
+    "email": "ldcolon@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Soto Tolentino, Wanda",
+    "email": "wsoto@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Jiménez Santoni, Marissa",
+    "email": "majimenez@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Sánchez Álvarez, Héctor J.",
+    "email": "hsanchez@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Santos Ortiz, Rafael",
+    "email": "rasantos@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Molina Pérez, Eliezer",
+    "email": "emolina@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "González Costa, Adrián",
+    "email": "adgonzalez@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Santiago Negrón, María de L.",
+    "email": "mlsantiago@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Álvarez Conde, Ada M.",
+    "email": "aalvarez@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Dalmau Santiago, José L.",
+    "email": "jldalmau@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Hernández Ortiz, Luis Javier",
+    "email": "luhernandez@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "Santiago Rivera, José A.",
+    "email": "jsantiago@senado.pr.gov",
+    "vote": ""
+  },
+  {
+    "name": "González Huertas, Marially",
+    "email": "magonzalez@senado.pr.gov",
+    "vote": ""
+  }
+];
