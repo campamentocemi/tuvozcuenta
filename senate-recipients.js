dd3b69a08f77a2e655c258f22358bebfb71357a2
campamentocemi <campamentocemi@gmail.com>
2026-05-15 23:21:20 -0400

    Update senate-recipients.js

--- senate-recipients.js
+++ senate-recipients.js
@@ -7,225 +7,141 @@ const SENATE_RECIPIENTS = [
   {
     "name": "Rodríguez Veve, Joanne",
     "email": "joarodriguez@senado.pr.gov",
-    "party": "IND",
-    "district": "Acumulación",
-    "position": "Senadora por Acumulación",
     "vote": ""
   },
   {
     "name": "Matías Rosario, Gregorio",
     "email": "gmatias@senado.pr.gov",
-    "party": "PNP",
-    "district": "Acumulación",
-    "position": "Portavoz - PNP / Senador por Acumulación",
     "vote": ""
   },
   {
     "name": "Rivera Schatz, Thomas",
     "email": "trivera@senado.pr.gov",
-    "party": "PNP",
-    "district": "Acumulación",
-    "position": "Presidente / Senador por Acumulación",
     "vote": ""
   },
   {
     "name": "Soto Aguilú, Roxanna I.",
     "email": "risoto@senado.pr.gov",
-    "party": "PNP",
-    "district": "Acumulación",
-    "position": "Senadora por Acumulación",
     "vote": ""
   },
   {
     "name": "Toledo López, Ángel",
     "email": "atoledo@senado.pr.gov",
-    "party": "PNP",
-    "district": "Acumulación",
-    "position": "Senador por Acumulación",
     "vote": ""
   },
   {
     "name": "Morales Rodríguez, Juan Oscar",
     "email": "jumorales@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito I - San Juan",
-    "position": "Portavoz Alterno - PNP / Senador por Distrito",
     "vote": ""
   },
   {
     "name": "Moran Trinidad, Nitza",
     "email": "nmoran@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito I - San Juan",
-    "position": "Senadora por Distrito",
     "vote": ""
   },
   {
     "name": "Padilla Alvelo, Migdalia",
     "email": "mpadilla@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito II - Bayamón",
-    "position": "Senadora por Distrito",
     "vote": ""
   },
   {
     "name": "Ríos Santiago, Carmelo J.",
     "email": "crios@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito II - Bayamón",
-    "position": "Primer Vicepresidente / Senador por Distrito",
     "vote": ""
   },
   {
     "name": "González López, Héctor Gabriel",
     "email": "hgonzalez@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito III - Arecibo",
-    "position": "Senador por Distrito",
     "vote": ""
   },
   {
     "name": "Pérez Soto, Brenda",
     "email": "bperez@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito III - Arecibo",
-    "position": "Senadora por Distrito",
     "vote": ""
   },
   {
     "name": "Román Rodríguez, Karen M.",
     "email": "kmroman@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito IV - Mayagüez-Aguadilla",
-    "position": "Senadora por Distrito",
     "vote": ""
   },
   {
     "name": "Rosa Ramos, Jeison",
     "email": "jrosa@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito IV - Mayagüez-Aguadilla",
-    "position": "Senador por Distrito",
     "vote": ""
   },
   {
     "name": "Barlucea Rodríguez, Jamie",
     "email": "jbarlucea@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito V - Ponce",
-    "position": "Senadora por Distrito",
     "vote": ""
   },
   {
     "name": "Reyes Berríos, Wilmer",
     "email": "wreyes@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito VI - Guayama",
-    "position": "Senador por Distrito",
     "vote": ""
   },
   {
     "name": "Colón La Santa, Luis Daniel",
     "email": "ldcolon@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito VII - Humacao",
-    "position": "Senador por Distrito",
     "vote": ""
   },
   {
     "name": "Soto Tolentino, Wanda",
     "email": "wsoto@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito VII - Humacao",
-    "position": "Senadora por Distrito",
     "vote": ""
   },
   {
     "name": "Jiménez Santoni, Marissa",
     "email": "majimenez@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito VIII - Carolina",
-    "position": "Segunda Vicepresidenta / Senadora por Distrito",
     "vote": ""
   },
   {
     "name": "Sánchez Álvarez, Héctor J.",
     "email": "hsanchez@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito VIII - Carolina",
-    "position": "Senador por Distrito",
     "vote": ""
   },
   {
     "name": "Santos Ortiz, Rafael",
     "email": "rasantos@senado.pr.gov",
-    "party": "PNP",
-    "district": "Distrito VI - Guayama",
-    "position": "Senador por Distrito",
     "vote": ""
   },
   {
     "name": "Molina Pérez, Eliezer",
     "email": "emolina@senado.pr.gov",
-    "party": "IND",
-    "district": "Acumulación",
-    "position": "Senador por Acumulación",
     "vote": ""
   },
   {
     "name": "González Costa, Adrián",
     "email": "adgonzalez@senado.pr.gov",
-    "party": "PIP",
-    "district": "Acumulación",
-    "position": "Portavoz Alterno - PIP / Senador por Acumulación",
     "vote": ""
   },
   {
     "name": "Santiago Negrón, María de L.",
     "email": "mlsantiago@senado.pr.gov",
-    "party": "PIP",
-    "district": "Acumulación",
-    "position": "Portavoz - PIP / Senadora por Acumulación",
     "vote": ""
   },
   {
     "name": "Álvarez Conde, Ada M.",
     "email": "aalvarez@senado.pr.gov",
-    "party": "PPD",
-    "district": "Acumulación",
-    "position": "Senadora por Acumulación",
     "vote": ""
   },
   {
     "name": "Dalmau Santiago, José L.",
     "email": "jldalmau@senado.pr.gov",
-    "party": "PPD",
-    "district": "Acumulación",
-    "position": "Pasado Presidente / Senador por Acumulación",
     "vote": ""
   },
   {
     "name": "Hernández Ortiz, Luis Javier",
     "email": "luhernandez@senado.pr.gov",
-    "party": "PPD",
-    "district": "Acumulación",
-    "position": "Portavoz - PPD / Senador por Acumulación",
     "vote": ""
   },
   {
     "name": "Santiago Rivera, José A.",
     "email": "jsantiago@senado.pr.gov",
-    "party": "PPD",
-    "district": "Acumulación",
-    "position": "Senador por Acumulación",
     "vote": ""
   },
   {
     "name": "González Huertas, Marially",
     "email": "magonzalez@senado.pr.gov",
-    "party": "PPD",
-    "district": "Distrito V - Ponce",
-    "position": "Portavoz Alterna - PPD / Senadora por Distrito",
     "vote": ""
   }
 ];
